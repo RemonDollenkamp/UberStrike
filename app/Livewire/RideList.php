34bec0089a8fc30c933eba406a3ae25fc4871470
Remon Dollenkamp <remon2004@outlook.com>
2024-01-24 11:59:41 +0100

    begin addride + delete ride

--- app/Livewire/RideList.php
+++ app/Livewire/RideList.php
@@ -9,11 +9,49 @@ class RideList extends Component
 {
     public $id;
 
+    public $start_point;
+    public $end_point;
+    public $personCount;
+    public $dep;
+
+
+    public $isOpen = false;
+
+    public function mount()
+    {
+        // Set the default value for personCount
+        $this->personCount = '';
+    }
     public function render()
     {
         $rides = Ride::all();
 
         return view('livewire.ride-list', ['rides' => $rides]);
+    }
+
+    public function saveDriver()
+    {
+        $this->validate([
+            'start_point' => 'required|string',
+            'end_point' => 'required|string',
+            'personCount' => 'required|int',
+            'dep' => 'required|date_format:Y-m-d\TH:i',
+        ]);
+
+        $databaseFormat = \Carbon\Carbon::parse($this->dep)->format('Y-m-d H:i:s');
+        $personCount = $this->personCount;
+
+        $ride = new Ride;
+        $ride->start_point = $this->start_point;
+        $ride->end_point = $this->end_point;
+        $ride->dep = $databaseFormat;
+
+        $ride->save();
+
+        // Add success message to the session
+        session()->flash('success', 'Rit toegevoegd');
+
+        return redirect()->route('taxiritten');
     }
 
     public function delete($id)
